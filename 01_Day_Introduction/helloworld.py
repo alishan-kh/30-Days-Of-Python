0ff7739eb0aa88deb6ed8e75b5488cea54ca9f72
Alishan Khowaja <77682078+alishan-kh@users.noreply.github.com>
2024-01-03 23:57:24 +0500

    Update helloworld.py
    Day 1 E2 and 3

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,66 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+############################################################################################################
+
+# ----- E2
+
+# 1
+
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+# 2
+name = 'Alishan'
+f_name = 'Khowaja'
+country = 'Pakistan'
+print('I am enjoying 30 days of python')
+print(name)
+print(f_name)
+print(country)
+
+#3
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(f_name))
+print(type(country))
+
+
+#--------- E3
+
+# 1 
+
+i = 10 #integer
+f= 9.8 #float
+comp = 3j #complex
+challenge = '30days' #string
+bool1 = True #boolean
+shopping = ['Ketchup', 'spices', 'mayo'] #list
+fruits = ('apple','banana','mango') #tuple
+myset = {'apple', 'banana', 'mango'} #set
+mydict = {
+  "name": "apple",  
+  "colour": "red",          #dictionary
+  "taste": "sweet",
+}
+
+# 2 
+
+# (2,3) = (x1,y1)
+# (10,8) = (x2,y2)
+
+print((((10-3)**2) + ((8-3)**2))**0.5)
+
