a502bac0bbee6548dedca7376332c064df6679e5
Alishan Khowaja <77682078+alishan-kh@users.noreply.github.com>
2024-01-04 18:54:20 +0500

    Update variables.py

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,58 @@
+# Day 2: 30 Days of python programming
 
-# Variables in Python
+f_name = 'Alishan'
+l_name = 'Khowaja'
+full_name = f_name + ' ' + l_name
+country = 'Pakistan'
+city = 'Karachi'
+age = 20
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+a, b ='1st alphabet', 2
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
-    }
+# exercise Lv2
+print(type(f_name))
+print(type(l_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
 
-# Printing the values stored in the variables
+print(len(f_name))
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+print('is my first name longer than my lat name?', len(f_name) > len(l_name))
 
-# Declaring multiple variables in one line
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+pi= 22/7
+# print(pi)
+r = 3
+area_of_circle = pi*(r**2)
+# print(area_of_circle)
+circum_of_circle = 2*pi*r
+# print(circum_of_circle)
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+r = int(input('Input a value for radius'))
+print('The area of the circle with radius ', r, 'is : ',(pi*(r**2)))
+
+inp_fname = input('Enter your first name')
+inp_lname = input('Enter your last name')
+inp_country = input('Enter your Country name')
+inp_age = int(input('Enter your Age'))
+
+help('keywords')
