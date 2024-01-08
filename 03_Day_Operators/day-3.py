cd42a365d24cdd01cdeb3966b16f15831b3ea01b
Alishan Khowaja <77682078+alishan-kh@users.noreply.github.com>
2024-01-08 21:35:33 +0500

    Update day-3.py

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -1,124 +1,76 @@
-# Arithmetic Operations in Python
-# Integers
+#Day 3: 30 Days of python programming
+age = 20
+height = 5.11
+num_complex = 4-4j
 
-print('Addition: ', 1 + 2)
-print('Subtraction: ', 2 - 1)
-print('Multiplication: ', 2 * 3)
-print ('Division: ', 4 / 2)                         # Division in python gives floating number
-print('Division: ', 6 / 2)
-print('Division: ', 7 / 2)
-print('Division without the remainder: ', 7 // 2)   # gives without the floating number or without the remaining
-print('Modulus: ', 3 % 2)                           # Gives the remainder
-print ('Division without the remainder: ', 7 // 3)
-print('Exponential: ', 3 ** 2)                     # it means 3 * 3
+#Enter Base and Height of a triangle
+triangle_base = float(input('Input Base of a triangle : '))
+triangle_height = float(input('Input Height of a triangle : '))
+print('Area of the triangle with entered Base and Height is : ',0.5*triangle_base*triangle_height)
 
-# Floating numbers
-print('Floating Number,PI', 3.14)
-print('Floating Number, gravity', 9.81)
+#Enter Sides of a triangle
+side_a = float(input('Input Side A : '))
+side_b = float(input('Input Side B : '))
+side_c = float(input('Input Side C : '))
+print('The perimeter of the triangle is : ',side_a+side_b+side_c)
 
-# Complex numbers
-print('Complex number: ', 1 + 1j)
-print('Multiplying complex number: ',(1 + 1j) * (1-1j))
+#Enter Sides of a Rectangle
+rect_len = float(input('Input rectangle length : '))
+rect_width = float(input('Input rectangle widht : '))
+print('The area of the rectangle is : ',rect_len*rect_width)
+print('The perimeter of the rectangle is : ',(2*rect_len)+(2*rect_width))
 
-# Declaring the variable at the top first
+#Enter Radius of a triangle
+pi= 3.14
+triangle_radius = float(input('Enter radius : '))
+print('the radius of the triangle is : ',pi*triangle_radius*triangle_radius)
+print('the circumference of the triangle is : ',2*pi*triangle_radius)
 
-a = 3 # a is a variable name and 3 is an integer data type
-b = 2 # b is a variable name and 3 is an integer data type
+m8 = 2
+m9 = (10-2)/(6-2)
+ed9 = (((6-2)**2)+((10-2)**2))**0.5
 
-# Arithmetic operations and assigning the result to a variable
-total = a + b
-diff = a - b
-product = a * b
-division = a / b
-remainder = a % b
-floor_division = a // b
-exponential = a ** b
+print('Is the slope in q9 greater than, less than or equal to the one in q8? ')
+if m9>m8:
+    print('Greater Than')
+elif m9==m8:
+    print('Equal to')
+else:
+    print('Less than')
 
-# I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
-print(total) # if you don't label your print with some string, you never know from where is  the result is coming
-print('a + b = ', total)
-print('a - b = ', diff)
-print('a * b = ', product)
-print('a / b = ', division)
-print('a % b = ', remainder)
-print('a // b = ', floor_division)
-print('a ** b = ', exponential)
+for x in range(-5,5):
+    if ((x**2)+(6*x)+9)==0:
+        print('The value of X for which y is 0 : ',x)
 
-# Declaring values and organizing them together
-num_one = 3
-num_two = 4
+print('length of the word "python" : ',len('python'))
+print('length of the word "dragon" : ',len('dragon'))
+print('length of the words python and dragon are not equal.',len('dragon')!=len('python'))
 
-# Arithmetic operations
-total = num_one + num_two
-diff = num_two - num_one
-product = num_one * num_two
-div = num_two / num_two
-remainder = num_two % num_one
+if 'on' in 'python' and 'dragon':
+    print('True, "on" is present in both')
+    
+if 'jargon' in 'I hope this course is not a jargon':
+    print('True, "Jargon" is present in the sentence')
 
-# Printing values with label
-print('total: ', total)
-print('difference: ', diff)
-print('product: ', product)
-print('division: ', div)
-print('remainder: ', remainder)
+if 'on' not in 'python' and 'dragon':
+    print('True, "on" is not present in both')
+else:
+    print('False, "on" is present in both')
 
+str(float(len('python')))
 
-# Calculating area of a circle
-radius = 10                                 # radius of a circle
-area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
-print('Area of a circle:', area_of_circle)
+#to check for even or odd, we use modulus 2 --> x%2, if the answer is 0 it means the number is even hence there is no remainder, else the number is odd.
 
-# Calculating area of a rectangle
-length = 10
-width = 20
-area_of_rectangle = length * width
-print('Area of rectangle:', area_of_rectangle)
+print((7//3)==int(2.7))
+print(type('10')==type(10))
+print(int(9.8)==10)
 
-# Calculating a weight of an object
-mass = 75
-gravity = 9.81
-weight = mass * gravity
-print(weight, 'N')
+hours = input('enter hours ')
+rph = input('enter rate per hour ')
+print('weekly earning is = ',int(rph)*int(hours))
 
-print(3 > 2)     # True, because 3 is greater than 2
-print(3 >= 2)    # True, because 3 is greater than 2
-print(3 < 2)     # False,  because 3 is greater than 2
-print(2 < 3)     # True, because 2 is less than 3
-print(2 <= 3)    # True, because 2 is less than 3
-print(3 == 2)    # False, because 3 is not equal to 2
-print(3 != 2)    # True, because 3 is not equal to 2
-print(len('mango') == len('avocado'))  # False
-print(len('mango') != len('avocado'))  # True
-print(len('mango') < len('avocado'))   # True
-print(len('milk') != len('meat'))      # False
-print(len('milk') == len('meat'))      # True
-print(len('tomato') == len('potato'))  # True
-print(len('python') > len('dragon'))   # False
+years = input('number of years youve lived ')
+print('youve lived for ',365*24*60*60*int(years), ' seconds')
 
-# Boolean comparison
-print('True == True: ', True == True)
-print('True == False: ', True == False)
-print('False == False:', False == False)
-print('True and True: ', True and True)
-print('True or False:', True or False)
-
-# Another way comparison 
-print('1 is 1', 1 is 1)                   # True - because the data values are the same
-print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
-print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
-print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
-print('coding' in 'coding for all') # True - because coding for all has the word coding
-print('a in an:', 'a' in 'an')      # True
-print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
-
-print(3 > 2 and 4 > 3) # True - because both statements are true
-print(3 > 2 and 4 < 3) # False - because the second statement is false
-print(3 < 2 and 4 < 3) # False - because both statements are false
-print(3 > 2 or 4 > 3)  # True - because both statements are true
-print(3 > 2 or 4 < 3)  # True - because one of the statement is true
-print(3 < 2 or 4 < 3)  # False - because both statements are false
-print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
-print(not True)      # False - Negation, the not operator turns true to false
-print(not False)     # True
-print(not not True)  # True
-print(not not False) # False
+for i in range(1,6):
+    print(f'{i} 1 {i} {i*i} {i*i*i}')
